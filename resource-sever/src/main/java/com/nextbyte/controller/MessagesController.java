8febcd8b651e11584dcee3d84367864b809476d0
徐进 <1451521857@qq.com>
2024-08-27 23:08:49 +0800

    Initial commit

---
+++ resource-sever/src/main/java/com/nextbyte/controller/MessagesController.java
@@ -0,0 +1,36 @@
+package com.nextbyte.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 由于接口资源地址没有在Spring Security配置中放开，因此三个接口访问都需要传入accessToken。
+ * 其中，messages1接口的要求只需传入accessToken，messages2接口要求传入accessToken和拥有profile权限，messages3接口要求传入accessToken和拥有Message权限，而message4接口是新增的一个，试试用户的这个权限访问结果
+ */
+@RestController
+public class MessagesController {
+
+    @GetMapping("/messages1")
+    public String getMessages1() {
+        return " hello Message 1";
+    }
+
+    @GetMapping("/messages2")
+    @PreAuthorize("hasAuthority('SCOPE_profile')")
+    public String getMessages2() {
+        return " hello Message 2";
+    }
+
+    @GetMapping("/messages3")
+    @PreAuthorize("hasAuthority('SCOPE_Message')")
+    public String getMessages3() {
+        return " hello Message 3";
+    }
+
+    @GetMapping("/messages4")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public String getMessages4() {
+        return " hello Message 4";
+    }
+}
